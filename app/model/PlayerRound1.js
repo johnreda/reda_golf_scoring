9b2607b9af6c91937d5ab5d89fb86944bbac8990
Ahmed Haque <afhaque@rice.edu>
2016-05-14 12:00:57 -0400

    Added files

--- app/model/PlayerRound1.js
+++ app/model/PlayerRound1.js
@@ -11,7 +11,7 @@ var Sequelize = require("sequelize");
 var sequelize = require("../config/connection.js"); 
 
 // "classes" model that matches up with DB
-var playerScores = sequelize.define("playerSCores", {
+var playerRound1 = sequelize.define("playerRound1", {
 	id: {
 		type: Sequelize.INTEGER,
 		autoIncrement: true,
@@ -20,7 +20,58 @@ var playerScores = sequelize.define("playerSCores", {
 	name: {
 		type: Sequelize.STRING
 	},
-	holeScores: {
+	hole1: {
+		type: Sequelize.INTEGER
+	},
+	hole2: {
+		type: Sequelize.INTEGER
+	},
+	hole3: {
+		type: Sequelize.INTEGER
+	},
+	hole4: {
+		type: Sequelize.INTEGER
+	},
+	hole5: {
+		type: Sequelize.INTEGER
+	},
+	hole6: {
+		type: Sequelize.INTEGER
+	},
+	hole7: {
+		type: Sequelize.INTEGER
+	},
+	hole8: {
+		type: Sequelize.INTEGER
+	},
+	hole9: {
+		type: Sequelize.INTEGER
+	},
+	hole10: {
+		type: Sequelize.INTEGER
+	},
+	hole11: {
+		type: Sequelize.INTEGER
+	},
+	hole12: {
+		type: Sequelize.INTEGER
+	},
+	hole13: {
+		type: Sequelize.INTEGER
+	},
+	hole14: {
+		type: Sequelize.INTEGER
+	},
+	hole15: {
+		type: Sequelize.INTEGER
+	},
+	hole16: {
+		type: Sequelize.INTEGER
+	},
+	hole17: {
+		type: Sequelize.INTEGER
+	},
+	hole18: {
 		type: Sequelize.INTEGER
 	},
 	grossScore: {
@@ -32,11 +83,11 @@ var playerScores = sequelize.define("playerSCores", {
 });
 
 // Syncs with DB
-playerScores.sync({}).then(function () {
+playerRound1.sync({}).then(function () {
   // Table created
   console.log('player scores table done syncing')
 });
 
 // Makes the classes Model available for other files (will also create a table)
-module.exports = playerScores;
+module.exports = playerRound1;
 
